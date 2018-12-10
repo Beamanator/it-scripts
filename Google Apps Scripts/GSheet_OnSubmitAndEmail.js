8fe5b4db2f496d1be4dc82e26e7b6072fa6303fb
Alex Beaman <dabeamanator@gmail.com>
2018-12-10 14:25:15 +0200

    Adding custom htmlBody to email

--- Google Apps Scripts/GSheet_OnSubmitAndEmail.js
+++ Google Apps Scripts/GSheet_OnSubmitAndEmail.js
@@ -39,17 +39,20 @@ function submit(eventObj) {
     const formFilledBy = namedValues[ getSubmitterEmailAddress() ];
 
     const email = {
-        name: 'New StARS Staff Form',
+        // name: 'New StARS Staff Form', // (not used if noReply is true)
         noReply: true,
         to: getHREmail(),
-        // cc: '...',
+        // cc: '...', // conditionally set below
         subject: 'New staff form submitted!',
-        body: 'New staff form just filled out by: ' + formFilledBy +
-            '\n' +
-            '\nReason for filling out form: ' + formReason +
-            '\n' +
-            '\nRemember to ask this person for an updated role description!',
+        // htmlBody: '...', // conditionally set below
     };
+  
+    // if promotion / transferred, send extra body text
+    if (formReason === 'Staff member being promoted OR transferring between programs') {
+        email['htmlBody'] = emailBodyIntro(formFilledBy, formReason) + programEmailBody() + HRemailBody();
+    } else {
+        email['htmlBody'] = emailBodyIntro(formFilledBy, formReason) + HRemailBody();
+    }
 
     // if volunteer, cc volunteer officer
     if (formReason === 'Volunteer') {
@@ -62,4 +65,36 @@ function submit(eventObj) {
     MailApp.sendEmail(email);
 
     Logger.log('done notifying HR of form filled');
+}
+
+// ================================ Email Templates ================================
+function emailBodyIntro(formFilledBy, formReason) {
+    return 'New staff form just filled out by: ' + formFilledBy
+        + '<br><br>Reason for filling out form: ' + formReason
+        + '<br><br>';
+}
+function programEmailBody() {
+    return '<strong>Reminder for program promoting / transferring staff member:</strong>'
+        + '<br><ul>'
+            + '<li>Job Description</li>'
+            + '<li>Start Date of new position</li>'
+            + '<li>Fill <a href="https://drive.google.com/open?id=1heYm2qy4Gog_usQbqRLmgnwA5ANcRg3Y">promotion / demotion form</a>'
+        + '</ul>';
+}
+function HRemailBody() {
+    return '<strong>Reminder for HR staff: Follow this checklist:</strong>'
+        + '<br><ul>'
+            + '<li>Prepare Contract to be signed</li>'
+            + '<li>Check references</li>'
+            + "<li>Start employee personnel file (including ID's)</li>"
+        + '</ul>'
+        + '<i>P.S. Email "it.team@stars-egypt.org" if you would like to edit this message :)</i>';
+}
+function testEmail() {
+    MailApp.sendEmail({
+        noReply: true,
+        to: 'abeaman@stars-egypt.org',
+        subject: 'New staff form submitted!',
+        htmlBody: emailBodyIntro('bob', 'cuz i want to') + programEmailBody() + HRemailBody(),
+    });
 }
